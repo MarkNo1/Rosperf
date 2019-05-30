d3c0398cd0232a608f1b4df9816ca2d69234a2a3
markno1 <marcotreglia1@gmail.com>
2019-05-30 11:13:43 +0000

    Nodelet - Receiver + v1 & v2 launchfiles

---
+++ include/rosperf/nodelets/Receiver.h
@@ -0,0 +1,105 @@
+/*!****************************************************************************
+ *  @file 	 Receiver.h
+ *  @date 	 2019.05.30
+ *  @author  M. Treglia
+ *  @brief
+ *****************************************************************************/
+
+#ifndef ROSNODELETS_RECEIVER_
+#define ROSNODELETS_RECEIVER_
+
+#include <ros/ros.h>
+// Declare a Ros Nodelet
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+// Base Template Class for nodelets
+#include <rosnodelets/template/BaseNodelet.h>
+// Message
+#include <rosperf/Frame.h>
+#include <rosperf/Byte.h>
+#include <rosperf/Signal.h>
+
+/**
+ *  Receiver namespace
+ */
+namespace rosnodelets {
+
+/**
+ *  Naming simplification
+ */
+//using :
+
+/**
+ * @class Receiver
+ * @brief
+ */
+class Receiver : public base::NodeletBM {
+ public:
+  using base::NodeletBM::Nodelet;
+
+    /**
+     * Initialization nodelet name
+     */
+    void InitializeNodeletName() final;
+
+  /**
+   * Initialization all components
+   */
+  void InitializeNodelet() final;
+
+  /**
+   * Loading ros parameters
+   */
+  void LoadRosParameters() final;
+
+  /**
+   * Establish ros communications
+   */
+  void EstablishRosCommunication() final;
+
+  /**
+   *  Callback to velodyne packets
+   */
+  inline void Callback(const rosperf::Frame::ConstPtr &frame);
+
+  /**
+   *  Callback to benchmark the nodelet
+   */
+  inline void CallbackBM(const rosperf::Frame::ConstPtr &frame);
+
+  /**
+   * Send
+   */
+  void Send(int code);
+
+  /**
+   * Reset
+   */
+  void Reset();
+
+  void Log(int status);
+
+ private:
+    // Communications
+    ros::Publisher _pub;
+    ros::Subscriber _sub;
+    // Params
+    int _last_sequence;
+    float _last_hz;
+    float _last_dim;
+    // Success param
+    int _success_counter;
+    int _success_threshold;
+    // Fail param
+    int _fail_counter;
+    int _fail_threshold;
+};
+
+}  // namespace rosnodelets
+
+/**
+ * Register the nodelet.
+ */
+PLUGINLIB_EXPORT_CLASS(rosnodelets::Receiver, nodelet::Nodelet);
+
+#endif  // ROSNODELETS_RECEIVER_
---
+++ src/nodelets/Receiver.cc
@@ -0,0 +1,98 @@
+/*!****************************************************************************
+ *  @file   Receiver.cc
+ *  @date   2019.05.30
+ *  @author M. Treglia
+ *  @brief  Definition Receiver nodelet
+ *****************************************************************************/
+
+#include <rosperf/nodelets/Receiver.h>
+
+namespace rosnodelets {
+
+    void Receiver::InitializeNodeletName() {SetName("Receiver");}
+
+    void Receiver::InitializeNodelet() {
+        _last_sequence = 0;
+        _last_hz = 0;
+        _last_dim = 0;
+        _success_counter = 0;
+        _success_threshold = 10;
+        _fail_counter = 0;
+        _fail_threshold = 3;
+        LogInfo("Initialized");
+    }
+
+    void Receiver::LoadRosParameters() {}
+
+    void Receiver::EstablishRosCommunication() {
+        _sub = getMTNodeHandle().subscribe("/Frames", 1, &Receiver::Callback, this, ros::TransportHints().tcpNoDelay());
+        _pub = getMTNodeHandle().advertise<rosperf::Signal>("/Reset", 1);
+    }
+
+    inline void Receiver::CallbackBM(const rosperf::Frame::ConstPtr &frame) {
+     Start();
+     Callback(frame);
+     End();
+    }
+
+    inline void Receiver::Callback(const rosperf::Frame::ConstPtr &frame) {
+        // Success
+        if ( _last_sequence == frame->sequence -1) {
+            _last_sequence = frame->sequence;
+            _last_hz = frame->hz;
+            _last_dim = sizeof(rosperf::Byte) * frame->payload.size() + 16;
+            _success_counter++;
+            // Success threshold times
+            if(_success_counter == _success_threshold) {
+                // Reset
+                Reset();
+                // Increase Hz
+                Send(rosperf::Signal::INCREASE_HZ);
+            }
+        }
+            // Fail
+        else {
+            // Copy last sequence
+            _last_sequence = frame->sequence;
+            // Increase fail case
+            _fail_counter++;
+            // Fail threshold times
+            if (_fail_counter == _fail_threshold) {
+                Log(3);
+                // Reset params
+                Reset();
+                // Increase Payload
+                Send(rosperf::Signal::INCREASE_DM);
+            }
+        }
+    }
+
+    void Receiver::Send(int code) {
+        // Create Signal msg
+        rosperf::Signal signal;
+        signal.signal = code;
+        // Publish
+        _pub.publish(signal);
+    }
+
+    void Receiver::Reset() {
+        _last_hz = 0;
+        _last_dim = 0;
+        _last_sequence = 0;
+        _success_counter = 0;
+        _fail_counter = 0;
+    }
+
+    void Receiver::Log(int status) {
+        // Warn
+        if (status == 1 )
+            ROS_WARN("-- sequence(%d) hz(%f) dim(%f byte) success(%d)", _last_sequence,_last_hz, _last_dim, _success_counter);
+        // Info
+        if (status == 2 )
+            ROS_INFO("-- sequence(%d) hz(%f) dim(%f byte) success(%d)", _last_sequence,_last_hz, _last_dim, _success_counter);
+        // Error
+        if (status == 3 )
+            ROS_ERROR("-- sequence(%d) hz(%f) dim(%f byte) success(%d)", _last_sequence,_last_hz, _last_dim, _success_counter);
+    }
+
+}  // namespace rosnodelets
